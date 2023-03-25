558da8312aaeb8f60665e351981c91ad8a2f89f3
pphila <efl2ice89@gmail.com>
2023-03-25 11:39:18 -0700

    add conditional for quantity amount warnings

--- src/components/Vinyl.js
+++ src/components/Vinyl.js
@@ -2,6 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export default function Vinyl(props) {
+  let inventoryWarn = null;
+  if (props.quantity < 1) {
+    inventoryWarn = <p>OUT OF STOCK!</p>
+  } else if (props.quantity <= 10 ) {
+    inventoryWarn = <p>Inventory Low!</p>
+  }
+  
   return (
     <>
       <div onClick={() => props.whenVinylClicked(props.id)}>
@@ -9,6 +16,7 @@ export default function Vinyl(props) {
         <h4><em>{props.album}</em></h4>
         <h4>{props.description}</h4>
         <h4>{props.quantity}</h4> {/*quantity amount will determine low stock or out of stock warning using conditionals?*/}
+        {inventoryWarn}
       </div>
       <button onClick={()=> props.whenSellClicked(props.id)}>Sell</button>
     </>
@@ -22,5 +30,6 @@ Vinyl.propTypes = {
   quantity: PropTypes.number,
   id: PropTypes.string,
   whenVinylClicked: PropTypes.func,
-  whenSellClicked: PropTypes.func
+  whenSellClicked: PropTypes.func,
+  inventoryWarn: PropTypes.bool
 };
